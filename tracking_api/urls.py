9f3dae30c98e750bff7c586a3514a081a7e9b7ff
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-12-02 20:20:58 +0100

    Django messages and Modify dashboards templates

--- tracking_api/urls.py
+++ tracking_api/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('about/#', views.privacy_policy, name='privacy_policy'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('register/', views.register, name='register'),
     path('accounts/login/', views.login_view, name='login'),  
     path('logout/', views.logout_view, name='logout'),
     path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('#/', views.privacy_policy, name='privacy_policy'),
+    
 ]
 
